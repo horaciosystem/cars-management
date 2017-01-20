6fe2988206d2e99c71591a171bf0d85b90ec4202
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2017-01-20 15:13:31 -0200

    Create webpack configuration file for production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  cache: false,
+  entry: [
+    './src/js/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false, // Suppress uglification warnings
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+      },
+      exclude: [/\.min\.js$/gi] 
+    }),  
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],        
+        include: path.join(__dirname, 'src')        
+      },
+      {
+        test: /\.scss$/,        
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.join(__dirname, 'src/css')
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader'
+      }
+    ]
+  }
+};
