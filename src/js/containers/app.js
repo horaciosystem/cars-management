2dd4cbfc68187067478550512f278951fffc1cea
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2017-01-19 23:36:00 -0200

    Add pagination component. Remove filter function of reducer

---
+++ src/js/containers/app.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import CarList from './car-list';
+import ToolBar from './toolbar';
+import MainHeader from '../components/main-header';
+import CarForm from './car-form';
+import Pagination from '../components/pagination';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {  
+  createCar,
+  updateCar,
+  removeCar,
+  loadCars,
+  filterCars
+} from '../reducers/cars-reducer';
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false,
+      carToUpdate: null
+    }
+
+    this.handleToogleModal = this.handleToogleModal.bind(this);
+    this.onCreateCar = this.onCreateCar.bind(this);
+    this.onUpdateCar = this.onUpdateCar.bind(this);
+    this.onDeleteCar = this.onDeleteCar.bind(this);
+  }
+
+  handleToogleModal(car) {
+    this.setState((prevState, props) => {
+      return {
+        modalIsOpen: !prevState.modalIsOpen,
+        carToUpdate: car
+      }
+    })
+  }
+
+  onCreateCar(car) {
+    this.props.createCar(car);
+    this.handleToogleModal(null);
+  }
+
+  onUpdateCar(car) {
+    this.props.updateCar(car);
+    this.hadleToogleModal(null);
+  }
+
+  onDeleteCar(carId) {
+    this.props.removeCar(carId);    
+  }
+
+  navigateToPage(page) {
+    this.props.loadCars(page);
+  }
+
+  render() {
+    const {modalIsOpen, carToUpdate } = this.state;
+    
+    return (
+      <div className="main-container">
+        <MainHeader />
+        <ToolBar 
+          toogleModal={this.handleToogleModal}
+        />
+        <CarList 
+          toogleModal={this.handleToogleModal}
+          onDelete={this.onDeleteCar}
+        />
+        {this.props.carsState.pagination.data.length > 0 &&
+          <Pagination 
+            carsState={this.props.carsState}
+            loadCars={(foo) => this.navigateToPage(foo)}
+          />
+        }
+        {this.state.modalIsOpen &&
+          <CarForm
+            title={carToUpdate ? 'Editar Veículo' : 'Cadastrar Veículo'}
+            onSubmit={carToUpdate ? this.onUpdateCar : this.onCreateCar}
+            initialValues={carToUpdate ? carToUpdate : null}
+            toogleModal={this.handleToogleModal}
+            modalIsOpen
+          />          
+        }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    carsState: state.cars
+  };
+}
+
+const mapDispatchToProps = dispatch => ({
+   createCar: bindActionCreators(createCar, dispatch),
+   updateCar: bindActionCreators(updateCar, dispatch),
+   removeCar: bindActionCreators(removeCar, dispatch),
+   loadCars: bindActionCreators(loadCars, dispatch),
+   filterCars: bindActionCreators(filterCars, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
